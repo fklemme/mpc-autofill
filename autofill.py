a1f89b47d82463eac4f22a03403d9dc6a8065655
Florian Klemme <mail@florianklemme.de>
2021-09-29 02:24:22 +0200

    Testing some options for "autofill" docker...

--- autofill.py
+++ autofill.py
@@ -525,6 +525,7 @@ if __name__ == "__main__":
 
     # Set up chrome driver window here to avoid tqdm issues
     chrome_options = Options()
+    chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--log-level=3")
     chrome_options.add_experimental_option("excludeSwitches", ["enable-logging"])
     chrome_options.add_experimental_option("detach", True)
